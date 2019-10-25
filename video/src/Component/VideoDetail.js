af8a26d2ba29b63933fc430ebe60d9f19645f9c2
anjana6 <anjanashakthi95@gmail.com>
2019-10-25 18:34:49 +0530

    fix the error

--- video/src/Component/VideoDetail.js
+++ video/src/Component/VideoDetail.js
@@ -10,7 +10,7 @@ const VideoDetail = (props) => {
     return(
     <div>
         <div className="ui embed">
-            <iframe src={videosrc}/>
+            <iframe title="video player" src={videosrc}/>
         </div>
         <div className="ui segment">
             <h4 className="ui header">{props.video.snippet.title}</h4>
--- video/src/Component/VideoList.js
+++ video/src/Component/VideoList.js
@@ -4,7 +4,7 @@ import VideoItem from './VideoItem'
 const VideoList = ({Videos,onVideoSelect}) => {
 
     const renderList = Videos.map((video) => {
-        return <VideoItem onVideoSelect={onVideoSelect} video = {video}/>;
+        return <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video = {video}/>;
     });
 
 
